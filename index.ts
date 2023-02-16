1c14a83082f402aa17d960a6147d7632f042c29c
Luis Ferreira <luis.ferreira@smartgreen.net>
2023-02-16 11:39:21 -0300

    CHANGE methods

--- index.ts
+++ index.ts
@@ -42,6 +42,21 @@ app
             return res.status(error.response.status).json(error.response.data);
         }
     })
+    .put("/conta/transferencia?idOrigem=:idOrigem&idDestino=:idDestino", async (req: any, res: any) => {
+        try {
+            const response = await axios.put(
+                `${sagaService}/conta/transferencia
+                ?idOrigem=${req.params.idOrigem}
+                &idDestino=${req.params.idDestino}`,
+                {
+                    ...req.body,
+                }
+            );
+            return res.json(response.data);
+        } catch (error: any) {
+            return res.status(error.response.status).json(error.response.data);
+        }
+    })
     .post("/cliente", async (req: any, res: any) => {
         try {
             const response = await axios.post(`${sagaService}/cliente`, {
@@ -52,32 +67,13 @@ app
             return res.status(error.response.status).json(error.response.data);
         }
     })
-    .get("/cliente/melhores/:idGerente", async (req: any, res: Response) => {
+    .put("/cliente/:id", async (req: any, res: Response) => {
         try {
-            const response = await axios.get(
-                `${sagaService}/melhores/${req.params.idGerente}`
-            );
-
-            return res.json(response.data);
-        } catch (error: any) {
-            return res.status(error.response.status).json(error.response.data);
-        }
-    })
-    .get("/cliente?cpf=:cpf", async (req: any, res: Response) => {
-        try {
-            const response = await axios.get(
-                `${sagaService}/cliente?cpf=${req.params.cpf}`
-            );
-
-            return res.json(response.data);
-        } catch (error: any) {
-            return res.status(error.response.status).json(error.response.data);
-        }
-    })
-    .get("/cliente/:idGerente", async (req: any, res: Response) => {
-        try {
-            const response = await axios.get(
-                `${sagaService}/cliente/${req.params.idGerente}`
+            const response = await axios.put(
+                `${sagaService}/cliente/${req.params.id}`,
+                {
+                    ...req.body,
+                }
             );
 
             return res.json(response.data);
@@ -105,9 +101,14 @@ app
             return res.status(error.response.status).json(error.response.data);
         }
     })
-    .get("/gerente", async (req: any, res: Response) => {
+    .put("/gerente/:id", async (req: any, res: Response) => {
         try {
-            const response = await axios.get(`${sagaService}/gerente`);
+            const response = await axios.put(
+                `${sagaService}/gerente/${req.params.id}`,
+                {
+                    ...req.body,
+                }
+            );
 
             return res.json(response.data);
         } catch (error: any) {
@@ -119,6 +120,51 @@ app
             const response = await axios.delete(
                 `${sagaService}/gerente/${req.params.id}`
             );
+            return res.json(response.data);
+        } catch (error: any) {
+            return res.status(error.response.status).json(error.response.data);
+        }
+    })
+
+    // COMPOSITION
+    // tem que arrumar ainda as chamadas
+    .get("/cliente/melhores/:idGerente", async (req: any, res: Response) => {
+        try {
+            const response = await axios.get(
+                `/melhores/${req.params.idGerente}`
+            );
+
+            return res.json(response.data);
+        } catch (error: any) {
+            return res.status(error.response.status).json(error.response.data);
+        }
+    })
+    .get("/cliente?cpf=:cpf", async (req: any, res: Response) => {
+        try {
+            const response = await axios.get(
+                `/cliente?cpf=${req.params.cpf}`
+            );
+
+            return res.json(response.data);
+        } catch (error: any) {
+            return res.status(error.response.status).json(error.response.data);
+        }
+    })
+    .get("/cliente/:idGerente", async (req: any, res: Response) => {
+        try {
+            const response = await axios.get(
+                `/cliente/${req.params.idGerente}`
+            );
+
+            return res.json(response.data);
+        } catch (error: any) {
+            return res.status(error.response.status).json(error.response.data);
+        }
+    })
+    .get("/gerente", async (req: any, res: Response) => {
+        try {
+            const response = await axios.get(`${sagaService}/gerente`);
+
             return res.json(response.data);
         } catch (error: any) {
             return res.status(error.response.status).json(error.response.data);
@@ -234,20 +280,6 @@ app
             return res.status(error.response.status).json(error.response.data);
         }
     })
-    .put("/cliente/:id", async (req: any, res: Response) => {
-        try {
-            const response = await axios.put(
-                `${clienteService}/${req.params.id}`,
-                {
-                    ...req.body,
-                }
-            );
-
-            return res.json(response.data);
-        } catch (error: any) {
-            return res.status(error.response.status).json(error.response.data);
-        }
-    })
 
     //GERENTE
     .get("/gerente/:id", async (req: any, res: Response) => {
@@ -265,20 +297,6 @@ app
         try {
             const response = await axios.get(
                 `${gerenteService}?email=${req.params.email}`
-            );
-
-            return res.json(response.data);
-        } catch (error: any) {
-            return res.status(error.response.status).json(error.response.data);
-        }
-    })
-    .put("/gerente/:id", async (req: any, res: Response) => {
-        try {
-            const response = await axios.put(
-                `${gerenteService}/${req.params.id}`,
-                {
-                    ...req.body,
-                }
             );
 
             return res.json(response.data);
